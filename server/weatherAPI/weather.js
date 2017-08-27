7e16da31b4baae2204a7e91ab8e562406892bdc0
sean0923 <iamseanhong@gmail.com>
2017-08-27 13:06:54 -0700

    Merge conflict solved

--- server/weatherAPI/weather.js
+++ server/weatherAPI/weather.js
@@ -5,14 +5,6 @@ try {
   config = require('../../config.js');
 } catch (e) {
   config = undefined;
-<<<<<<< HEAD
-=======
-var config;
-if ( process.env.NODE_ENV !== 'production' ) { // if we are not at heroku
-  var config = require('../../config.js');
->>>>>>> Fig bug related to require config.js
-=======
->>>>>>> Fix heroku bug
 }
 
 var requestWeather = function(geoCode, date, callback) {
