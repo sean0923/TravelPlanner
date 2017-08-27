a3e316de4ce6dd4eebb1f623cf434b5eb8fd803b
sean0923 <iamseanhong@gmail.com>
2017-08-26 17:50:42 -0700

    Fix heroku bug

--- server/geolocationAPI/geolocation.js
+++ server/geolocationAPI/geolocation.js
@@ -1,14 +1,9 @@
 var request = require('request');
 var config;
-<<<<<<< HEAD
 try {
   config = require('../../config.js');
 } catch (e) {
   config = undefined;
-=======
-if ( process.env.NODE_ENV !== 'production' ) { // if we are not at heroku
-  var config = require('../../config.js');
->>>>>>> 1c06d97c41c2b1591d85dba55d59ef3769b3bb86
 }
 
 var requestGeolocation = function(location, callback) {
--- server/yelpfood/yelpfood.js
+++ server/yelpfood/yelpfood.js
@@ -1,14 +1,9 @@
 const yelp = require('yelp-fusion');
 var config;
-<<<<<<< HEAD
 try {
   config = require('../../config.js');
 } catch (e) {
   config = undefined;
-=======
-if ( process.env.NODE_ENV !== 'production' ) { // if we are not at heroku
-  var config = require('../../config.js');
->>>>>>> 1c06d97c41c2b1591d85dba55d59ef3769b3bb86
 }
 
 var searchFood = function(searchCity, callback) {
