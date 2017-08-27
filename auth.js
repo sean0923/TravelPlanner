c4d9639cb7f5fa52167ee1f7bb23fc22b4bf7ce3
sean0923 <iamseanhong@gmail.com>
2017-08-26 17:54:04 -0700

    Change local url to heroku url

--- auth.js
+++ auth.js
@@ -2,6 +2,6 @@ module.exports = {
   'facebookAuth': {
     'clientID': '997962483679220',
     'clientSecret': 'c69c643c037e2685f7bb8c6018b0bacc',
-    'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+    'callbackURL': 'http://legacy-angular-angels.herokuapp.com/auth/facebook/callback'
   }
 }
